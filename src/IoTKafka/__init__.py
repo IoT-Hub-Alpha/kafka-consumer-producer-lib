786ddb3f963386f79e184dbbb30a75c6a3eb9817
Anton Koluh <antonkoluh1@gmail.com>
2026-03-11 10:45:44 +0200

    added package

--- src/IoTKafka/__init__.py
+++ src/IoTKafka/__init__.py
@@ -1,3 +1,4 @@
 from .kafka_configs import KafkaSettings, build_producer_config
+from .producer import KafKaProducer
 
-__all__ = ["KafkaSettings", "build_producer_config"]
+__all__ = ["KafkaSettings", "build_producer_config", "KafKaProducer"]
