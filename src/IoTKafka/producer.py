848be8a991a129382060cc86b83c12f1126dcc08
Anton Koluh <antonkoluh1@gmail.com>
2026-03-11 13:54:34 +0200

    producer update

--- src/IoTKafka/producer.py
+++ src/IoTKafka/producer.py
@@ -1,27 +1,34 @@
-from .producer_singleton import KafkaProducerManager
 from typing import Any, Callable, Optional
+from confluent_kafka import Producer, KafkaError, Message
 
-from confluent_kafka import KafkaError, Producer, Message  # type: ignore
+from .producer_singleton import KafkaProducerManager
 
 
 class IoTProducerException(Exception):
     pass
 
-class KafKaProducer:
-    def __init__(self, *_, **kwargs: dict[str, Any]):
-        self._producer = KafkaProducerManager.get_single_producer(**kwargs)  # type: ignore
 
-    def get_producer(self) -> Producer:
+class IoTProducer:
+    def __init__(self, **kwargs: dict[str, Any]):
+        self._producer: Producer = KafkaProducerManager.get_single_producer(**kwargs)
+
+    # expose real producer if needed
+    def raw(self) -> Producer:
         return self._producer
 
+    # custom produce with retry
     def produce(
         self,
         topic: str,
-        key: bytes,
+        key: bytes | str,
         value: bytes,
-        on_delivery: Callable[[Optional["KafkaError"], "Message"], None], # type: ignore
+        on_delivery: Callable[[Optional[KafkaError], Message], None],
         attempts: int = 3,
-    ):
+    ) -> None:
+
+        if isinstance(key, str):
+            key = key.encode("utf-8")
+
         for attempt in range(attempts):
             try:
                 self._producer.produce(
@@ -31,8 +38,13 @@ class KafKaProducer:
                     on_delivery=on_delivery,
                 )
                 self._producer.poll(0)
-                break
+                return
+
             except BufferError as e:
-                if attempt >= attempts:
-                    raise IoTProducerException(f"Kafka buffer full {e}")
+                if attempt == attempts - 1:
+                    raise IoTProducerException(f"Kafka buffer full: {e}")
+
                 self._producer.poll(0.5)
+
+    def __getattr__(self, name):
+        return getattr(self._producer, name)
