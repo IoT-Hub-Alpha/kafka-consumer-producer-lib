f5194a4fbe918aeab263195cba188c8a325efa0d
Anton Koluh <antonkoluh1@gmail.com>
2026-03-13 09:52:07 +0200

    fix bug

--- src/IoTKafka/consumer.py
+++ src/IoTKafka/consumer.py
@@ -1,9 +1,10 @@
 from confluent_kafka import Consumer, KafkaError
+from .kafka_configs import build_consumer_config
 
 
 class IoTKafkaConsumer:
     def __init__(self, **kwargs):
-        self._consumer = self._build_consumer_config(**kwargs)
+        self._consumer = build_consumer_config(**kwargs)
 
     def __getattr__(self, name):
         return getattr(self._consumer, name)
